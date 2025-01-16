6b3eef390206889546ca043d2b959977b53c5148
Gameoholic <viner.atmic@gmail.com>
2025-01-16 13:34:09 +0200

    add shotgun weapon class

---
+++ Source/Blaster/Weapons/ShotgunWeapon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapons/ProjectileWeapon.h"
+#include "ShotgunWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API AShotgunWeapon : public AProjectileWeapon
+{
+	GENERATED_BODY()
+	
+};
