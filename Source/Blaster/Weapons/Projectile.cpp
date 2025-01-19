b81c98b27b5e15cd66c1fa473d8a241d0eff9414
Gameoholic <viner.atmic@gmail.com>
2025-01-19 11:05:58 +0200

    adjust radial damage values

--- Source/Blaster/Weapons/Projectile.cpp
+++ Source/Blaster/Weapons/Projectile.cpp
@@ -113,6 +113,8 @@ void AProjectile::DestroyTimerFinished()
 
 void AProjectile::DoRadialDamage()
 {
+	DrawDebugSphere(GetWorld(), GetActorLocation(), RadialDamageInnerRadius, 10, FColor::Red, false, 2.0f);
+	DrawDebugSphere(GetWorld(), GetActorLocation(), RadialDamageOuterRadius, 10, FColor::Blue, false, 2.0f);
 	APawn* FiringPawn = GetInstigator();
 	if (FiringPawn && HasAuthority())
 	{
